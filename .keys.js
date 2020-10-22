6c9a1d25b84777d7cdd4952c0ffcd78eaed77def
Adriaan van der Bergh <avanderbergh@gmail.com>
2020-10-22 11:48:38 +0000

    Added new key

---
+++ .keys.js
@@ -0,0 +1,2 @@
+export const ipifyKey = "at_1OJPF1zRauXDR77H7gtbI0vOf0KVh";
+export const mapBoxKey = "pk.eyJ1IjoiYXZhbmRlcmJlcmdoIiwiYSI6ImNrZ2tyaGFibzA1OWczMG82anpkZGxobHYifQ.KQ-5dBPbWb8wRwRLpUDiiw";
